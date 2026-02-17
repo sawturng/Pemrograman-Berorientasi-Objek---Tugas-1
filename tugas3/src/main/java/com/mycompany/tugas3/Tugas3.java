b88603118762db60f8a7aa5005e935a9e823e41d
sawturng <anggiadhinihanifa@upi.edu>
2026-02-17 20:17:40 +0700

    Add files via upload
    Buatlah algoritma dalam bahasa Java, menjumlahkan kode ASCII dari karakter ‘1’ dan
    karakter ‘2’!

---
+++ tugas3/src/main/java/com/mycompany/tugas3/Tugas3.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugas3;
+
+/**
+ *
+ * @author My MSI
+ */
+public class Tugas3 {
+
+    public static void main(String[] args) {
+
+        char karakter1 = '1';
+        char karakter2 = '2';
+
+        int ascii1 = karakter1;
+        int ascii2 = karakter2;
+
+        int jumlah = ascii1 + ascii2;
+
+        System.out.println("ASCII karakter 1 : " + ascii1);
+        System.out.println("ASCII karakter 2 : " + ascii2);
+        System.out.println("Jumlah ASCII : " + jumlah);
+
+    }
+}
